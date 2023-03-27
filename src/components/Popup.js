9b63cf0f5f33a076863025c1763143a6a915937d
Kristina Ushakova <mramornayaa@yandex.ru>
2023-03-27 18:40:33 +0300

    Section

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,13 +1,13 @@
 export default class Popup {
   constructor(popupSelector) {
-      this._popup = document.querySelector(popupSelector);
-      this._closeButton = this._popup.querySelector('.popup__close')
+    this._popup = document.querySelector(popupSelector);
+    this._closeButton = this._popup.querySelector('.popup__close')
   }
 
 // Универсальная функция открытия всех popup
-  open () {
-      this._popup.classList.add('popup_opened');
-      this._setOpenEventListeners();
+  open() {
+    this._popup.classList.add('popup_opened');
+    this._setOpenEventListeners();
   }
 
   // Универсальная функция закрытия всех popup по нажатию на esc, overlay, крестик
@@ -15,30 +15,30 @@ export default class Popup {
     this._popup.classList.remove('popup_opened');
     document.removeEventListener('keydown', this._handleEscClose);
     this._popup.removeEventListener('mousedown', this._handleClickOverlay);
-}
+  }
 
 //Функция закрытия по esc
-_handleEscClose = (evt) => {
-  if (evt.key === 'Escape') {
+  _handleEscClose = (evt) => {
+    if (evt.key === 'Escape') {
       this.close();
-  }
-};
+    }
+  };
 
 //Функция закрытия по overlay
   _handleClickOverlay = (evt) => {
-      if (evt.target.classList.contains('popup')) {
-          this.close();
-      }
+    if (evt.target.classList.contains('popup')) {
+      this.close();
+    }
   };
 
 //Функция закрытия попапа по иконке
   _setOpenEventListeners() {
-      document.addEventListener('keydown', this._handleEscClose);
-      this._popup.addEventListener('mousedown', this._handleClickOverlay);
+    document.addEventListener('keydown', this._handleEscClose);
+    this._popup.addEventListener('mousedown', this._handleClickOverlay);
   }
 
 //Функция закрытия на крестик
   setEventListeners() {
-      this._closeButton.addEventListener('click', () => {this.close()});
+    this._closeButton.addEventListener('click', () => {this.close()});
   }
 }
